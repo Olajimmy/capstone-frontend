0755e01c2a981a95c218b7d2b37a4b6882479d9e
olajimmy <overdedicated9@gmail.com>
2025-01-21 13:34:13 -0500

    edit button fully functional

--- src/pages/JobPostPage.jsx
+++ src/pages/JobPostPage.jsx
@@ -1,11 +1,13 @@
+//********************Boundary */
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { getUser } from "../utilities/users-services";
 
 function JobPostPage() {
-  //const current = new Date();
-  //const curreDate=`${current.getMonth}`
+  const [currentUser, setCurrentUser] = useState(getUser());
   const [entries, setEntries] = useState([]);
   const [formData, setFormData] = useState({
+    email: currentUser.email,
     jobType: "",
     jobDescription: "",
     payRange: "",
@@ -14,7 +16,12 @@ function JobPostPage() {
     phone: "",
   });
   //
+  const [editButton, setEditButton] = useState(false);
   const [editingData, setEditingData] = useState({});
+  const [freshId, setFreshId] = useState("");
+  const [textData, setTextData] = useState({
+    jobType: "",
+  });
 
   const [update, setUpdate] = useState("nothing changed");
   //const [entType, setEntType] = useState("");
@@ -66,7 +73,7 @@ function JobPostPage() {
 
   //
   useEffect(() => {
-    getEntries();
+    getEntries(), currentUser;
   }, [update]);
   //
 
@@ -80,90 +87,18 @@ function JobPostPage() {
       setUpdate(`delete failed`);
     }
   };
-  //
-  const editEntry = async (id) => {
-    try {
-      const response = await axios.put(`${LOCAL_URL}/api/jobpost/${id}`);
-      // addEntry(id);
-      //   setFormData({
-      //     entryDate: [response.data.entryDate],
-      //     entryType: [response.data.entryType],
-      //     description: [response.data.description],
-      //   });
 
-      //   console.log(`edit`, response.data);
-
-      //   let newData = response.data;
-      //   console.log(
-      //     "entry date",
-      //     newData.entryDate,
-      //     "entry type:",
-      //     newData.entryType
-      //   );
-      //   setFormData({
-      //     entryDate: newData.entryDate,
-      //     entryType: newData.entryType,
-      //     description: newData.description,
-      //   });
-      //   console.log(formData);
-
-      //setFormData();
-      setUpdate(`edited data ${id} successfully`);
-    } catch (err) {
-      console.error(err);
-      setUpdate(`edit failed`);
-    }
-  };
-
-  const handleEdit = async (e, id) => {
-    console.log(`editing .... entry`);
-    editEntry(id);
-
-    //add in a function editEntry
-    // console.log(id);
-    // const response = await axios.get(`${LOCAL_URL}/api/braindump/${id}`);
-    // // console.log(" edit get", response.data);
-    // const newVal = response.data;
-    // console.log(newVal);
-    // const newValues = [...formData, id]; // Copy the current values array
-    // //newValues[i] = event.target.value;
-    // console.log(newValues[id]); //shows the text value in it
-    //setFormData(newValues[id]);
-    // setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSave = async (e, id) => {
+  const handleHired = async (e, id) => {
     console.log("save is clicked:", id);
     try {
       const response = await axios.get(`${LOCAL_URL}/api/jobpost/`);
       //console.log("response in save", response.data);
-      //   let newRes = response.data;
-      //   console.log(newRes);
-      //   setEditingData(newRes);
-      //   console.log("editing data", editingData);
-      //   editingData.map((i, index) => {
-      //     console.log(editingData[index]._id);
-      //     if (id === editingData[index]._id) {
-      //       console.log("we matched", id);
-      //       setFormData;
-      //       //setFormData({ ...formData, [e.target.name]: e.target.value });
+
       setFormData({ ...formData, [e.target.name]: e.target.value });
       console.log(formData);
-      //       //handleSubmit();
-      //       //   handleChange();
-      //       //   handleTypeSelect();
-      //     }
-      //   });
-      //   if (editingData == id) {
-      //     console.log("id matched");
-      //   } else {
-      //     console.log("not matched");
-      //   }
-      //setEntries(response.data);
     } catch (err) {
       console.error(err);
     }
-    //setFormData({ ...formData });
   };
 
   //
@@ -190,6 +125,7 @@ function JobPostPage() {
     setFormData({ ...formData });
     console.log(formData);
     newEntry = {
+      email: formData.email,
       jobType: formData.jobType,
       jobDescription: formData.jobDescription,
       payRange: formData.payRange,
@@ -215,6 +151,47 @@ function JobPostPage() {
   const loading = () => {
     return <h3>there dont seem to be an entries yet</h3>;
   };
+  //
+  const handleEditChange = (e) => {
+    setTextData({ ...textData, [e.target.name]: e.target.value });
+    console.log(textData);
+  };
+  //
+  const saveEntry = async (e, id, entry) => {
+    let newValue = {};
+    newValue = {
+      jobType: textData.jobType || entry.jobType,
+      jobDescription: textData.jobDescription || entry.jobDescription,
+      payRange: textData.payRange || entry.payRange,
+      comments: textData.comments || entry.comments,
+      businessAddress: textData.businessAddress || entry.businessAddress,
+      phone: textData.phone || entry.phone,
+    };
+    try {
+      const response = await axios.put(
+        `${LOCAL_URL}/api/jobpost/${id}`,
+        newValue
+      );
+      console.log(id);
+
+      //setFormData();
+      setUpdate(`edited data ${id} successfully`);
+    } catch (err) {
+      console.error(err);
+      setUpdate(`edit failed`);
+    }
+  };
+
+  //
+  const editAction = (entry) => {
+    if (editButton === false) {
+      setEditButton(true);
+      setEditingData(entry);
+    } else {
+      setEditButton(false);
+    }
+  };
+  //
 
   const loaded = () => {
     return (
@@ -226,48 +203,198 @@ function JobPostPage() {
           border: "1px solid white",
         }}
       >
+        {" "}
+        <h3>Job Post History</h3>
+        {freshId}
         {entries.map((entry) => {
           return (
             <>
-              <li style={{ border: "1px solid white" }}>
-                {entry.jobType}: {entry.jobDescription} : {entry.payRange}
-                <button
-                  onClick={(e) => {
-                    handleEdit(e, entry._id);
-                  }}
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={(e) => {
-                    handleSave(e, entry._id);
-                  }}
-                >
-                  Save
-                </button>
-                <button onClick={(e) => handleDelete(e, entry._id)}>
-                  Delete
-                </button>
-              </li>
+              {currentUser.email === entry.email ? (
+                <li key={entry._id} className="jobPostList">
+                  {entry.jobType}: {entry.jobDescription} - {entry.payRange}
+                  <button onClick={editAction}>Edit</button>
+                  {editingData._id === entry._id && (
+                    <form>
+                      <label>Job Type:</label>
+                      {/* <input
+                        type="text"
+                        name="jobType"
+                        value={editingData.jobType || entry.jobType}
+                        onChange={handleEditChange}
+                      /> */}
+                      <input
+                        type="text"
+                        name="jobType"
+                        onChange={handleEditChange}
+                        value={textData.jobType}
+                      />
+                      <br />
+                      <label>Job Description</label>
+
+                      <input
+                        type="text"
+                        name="jobDescription"
+                        onChange={handleEditChange}
+                        value={textData.jobDescription}
+                      />
+                      <br />
+                      <label>Pay Range</label>
+
+                      <input
+                        type="text"
+                        name="payRange"
+                        onChange={handleEditChange}
+                        value={textData.payRange}
+                      />
+                      <br />
+                      <label>Comments</label>
+
+                      <input
+                        type="text"
+                        name="comments"
+                        onChange={handleEditChange}
+                        value={textData.comments}
+                      />
+                      <br />
+                      <label>Business Address</label>
+
+                      <input
+                        type="text"
+                        name="businessAddress"
+                        onChange={handleEditChange}
+                        value={textData.businessAddress}
+                      />
+                      <br />
+                      <label>Phone Number</label>
+
+                      <input
+                        type="text"
+                        name="phone"
+                        onChange={handleEditChange}
+                        value={textData.phone}
+                      />
+                      <br />
+                      <br />
+                      <button onClick={(e) => saveEntry(e, entry._id, entry)}>
+                        Save
+                      </button>
+                    </form>
+                  )}
+                  <button onClick={(e) => handleDelete(e, entry._id)}>
+                    Delete
+                  </button>
+                </li>
+              ) : null}
+              {/* {currentUser.email === entry.email ? (
+                <>
+                  {" "}
+                  <li key={entry._id} style={{ border: "1px solid white" }}>
+                    {entry.jobType}: {entry.jobDescription} : {entry.payRange}
+                    <button onClick={() => editAction(entry._id)}>
+                      {editButton === false && freshId === currentUser._id
+                        ? "Edit"
+                        : "Hide"}
+                    </button>
+                    {}
+                    {editButton === true ? (
+                      <>
+                        <form>
+                          <label>Job type</label>
+                          <input
+                            type="text"
+                            name="jobType"
+                            onChange={handleEditChange}
+                            value={textData.jobType}
+                          />
+                          <br />
+                          <label>Job Description</label>
+
+                          <input
+                            type="text"
+                            name="jobDescription"
+                            onChange={handleEditChange}
+                            value={textData.jobDescription}
+                          />
+                          <br />
+                          <label>Pay Range</label>
+
+                          <input
+                            type="text"
+                            name="payRange"
+                            onChange={handleEditChange}
+                            value={textData.payRange}
+                          />
+                          <br />
+                          <label>Comments</label>
+
+                          <input
+                            type="text"
+                            name="comments"
+                            onChange={handleEditChange}
+                            value={textData.comments}
+                          />
+                          <br />
+                          <label>Business Address</label>
+
+                          <input
+                            type="text"
+                            name="businessAddress"
+                            onChange={handleEditChange}
+                            value={textData.businessAddress}
+                          />
+                          <br />
+                          <label>Phone Number</label>
+
+                          <input
+                            type="text"
+                            name="phone"
+                            onChange={handleEditChange}
+                            value={textData.phone}
+                          />
+                          <br />
+                          <button
+                            onClick={(e) => {
+                              saveEntry(e, entry._id);
+                            }}
+                          >
+                            Save
+                          </button>
+                        </form>
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                    <button
+                      onClick={(e) => {
+                        handleHired(e, entry._id);
+                      }}
+                    >
+                      Hired
+                    </button>
+                    <button onClick={(e) => handleDelete(e, entry._id)}>
+                      Delete
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <></>
+              )} */}
             </>
           );
         })}
       </ul>
     );
   };
+  //
 
+  //
   return (
     <>
       <h2>Job Post Page</h2>
-      <ol>
-        CRUD
-        <li>Create -form to add a new entry</li>
-        <li>Read - show all (which i have ), the rest is future work</li>
-        <li>Update- form to edit a specific entry</li>
-        <li>Delete - button to delete an entry</li>
-      </ol>
-      <div style={{ display: "flex" }}>
-        <div>
+      {currentUser.email}
+
+      <div style={{ display: "flex", border: "1px solid black" }}>
+        <div style={{ border: "1px solid black" }}>
           <h3>Add a new Job</h3>
           <form onSubmit={handleSubmit}>
             <label>
@@ -345,4 +472,3 @@ function JobPostPage() {
   );
 }
 export default JobPostPage;
-//
